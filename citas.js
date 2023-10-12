4bee96aa8a4ac395593495c5f7e219463893b344
pedro3015 <44181683+pedro3015@users.noreply.github.com>
2023-10-12 12:23:47 -0400

    Primer commit
    Se subuieron todos los archivos del programa

---
+++ citas.js
@@ -0,0 +1,83 @@
+const citas = [
+  {
+    autor: "Pau Casals",
+    texto:
+      "Nosotros debemos pensar que somos una de las hojas de un árbol, y el árbol es toda la humanidad. No podemos vivir los unos sin los otros, sin el árbol.",
+  },
+  {
+    autor: "Anonimo",
+    texto:
+      "Tener un hijo, plantar un árbol y escribir un libro es fácil. Lo difícil es criar un niño, regar el árbol y que alguien lea el libro",
+  },
+  {
+    autor: "Félicité Robert de Lamennais",
+    texto: "El pasado es como una lámpara colocada a la entrada del porvenir.",
+  },
+  {
+    autor: "Anonimo",
+    texto:
+      "Valor es lo que se necesita para levantarse y hablar, pero también es lo que se requiere para sentarse y escuchar.",
+  },
+  {
+    autor: "Séneca",
+    texto:
+      "La vida es una obra teatral que no importa cuánto haya durado, sino lo bien que haya sido representada.",
+  },
+  {
+    autor: "Arthur Schnitzler",
+    texto:
+      "Estar preparado es importante, saber esperar lo es aún más, pero aprovechar el momento adecuado es la clave de la vida.",
+  },
+  {
+    autor: "Dale Carnegie",
+    texto:
+      "Recuerda que la felicidad no depende en quién eres o qué tienes; depende únicamente en lo que piensas.",
+  },
+  {
+    autor: "Alejandro Jodorowsky",
+    texto:
+      "Lo que doy, me lo doy. Lo que no doy, me lo quito. Nada para mí que no sea para los otros.",
+  },
+  {
+    autor: "Ramón J. Sénder",
+    texto:
+      "¡Qué pequeñas son mis manos…! En relación con todo lo que la vida ha querido darme.",
+  },
+  {
+    autor: "Robert Louis Stevenson",
+    texto:
+      "Tanta prisa tenemos por hacer, escribir y dejar oír nuestra voz en el silencio de la eternidad, que olvidamos lo único realmente importante: ¡VIVIR!",
+  },
+  {
+    autor: "Frank Clark",
+    texto:
+      "Todo el mundo trata de realizar algo grande, sin darse cuenta de que la vida se compone de cosas pequeñas.",
+  },
+  {
+    autor: "Antoine de Saint-Exupéry",
+    texto:
+      "La perfección se logra no cuando no hay nada más que añadir, sino cuando no hay nada más que quitar.",
+  },
+  {
+    autor: "Héctor Tassinari",
+    texto: "Cada hombre puede mejorar su vida mejorando su actitud.",
+  },
+  {
+    autor: "Sophie Soynonov",
+    texto:
+      "Esperamos que pueda suceder cualquier cosa, y nunca estamos prevenidos para nada.",
+  },
+  {
+    autor: "Anonimo",
+    texto:
+      "No busques personas con tus mismos gustos, busca personas con tus mismos valores.",
+  },
+  {
+    autor: "Anonimo",
+    texto: "Cuando no sepas dónde ir, sigue el perfume de un sueño.",
+  },
+  {
+    autor: "Muhamed Alí",
+    texto: "No cuentes los días, haz que los días cuenten.",
+  },
+];
